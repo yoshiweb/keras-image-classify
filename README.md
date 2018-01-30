abd4d2601f086d1d40ab73a7024bb85c3343645f
yoshiweb <yoshiweb@gmail.com>
2018-01-30 18:17:57 +0900

    複数の画像判別に対応

--- README.md
+++ README.md
@@ -51,9 +51,19 @@ $ python keras_cnn_train.py
 
 ```
 $ python keras_cnn_predict.py test.jpg
-```
+Using TensorFlow backend.
+2018-01-30 18:15:45.143201: W tensorflow/core/platform/cpu_feature_guard.cc:45] The TensorFlow library wasn't compiled to use SSE4.2 instructions, but these are available on your machine and could speed up CPU computations.
+2018-01-30 18:15:45.143224: W tensorflow/core/platform/cpu_feature_guard.cc:45] The TensorFlow library wasn't compiled to use AVX instructions, but these are available on your machine and could speed up CPU computations.
+2018-01-30 18:15:45.143229: W tensorflow/core/platform/cpu_feature_guard.cc:45] The TensorFlow library wasn't compiled to use AVX2 instructions, but these are available on your machine and could speed up CPU computations.
+2018-01-30 18:15:45.143233: W tensorflow/core/platform/cpu_feature_guard.cc:45] The TensorFlow library wasn't compiled to use FMA instructions, but these are available on your machine and could speed up CPU computations.
+[[  4.40120971e-08   4.55074513e-07   9.99999523e-01]]
 
-> [[  3.21912841e-04   3.79991432e-15   9.99678135e-01]] ← 各ラベルの予測結果  
-> 2  ← 検定結果
+各ラベルの確率
+0.0000000440
+0.0000004551
+0.9999995232
+一番大きいラベルを予測結果として判定
+2
+```
 
 この場合「みかん」の可能性が一番高いと判断した。
--- keras-image-classify/keras_cnn_predict.py
+++ keras-image-classify/keras_cnn_predict.py
@@ -1,39 +1,89 @@
+#!/usr/bin/env python
+
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from PIL import Image
 import sys
 
-# 画像サイズ
-image_w = 32
-image_h = 32
-
-# 引数から画像パスを取得
-if len(sys.argv) != 2:
-    print("引数に判別する画像のパスを記載してください")
-    sys.exit(1)
-
-filename = sys.argv[1]
-print('input:', filename)
-
-# 画像を読み込み
-img = Image.open(filename) # PIL (Pillow(Python Imaging Library))で開く
-img = img.convert("RGB") # RGB
-img = img.resize((image_w, image_h)) # リサイズ
-
-# numpyのarrayにする
-x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
-x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
-x /= 255  # [0-255]の値を[0.0-1.0]に変換
+class ImageClassify:
 
 
-# 学習済みモデルの読み込み
-model = load_model('keras_cnn_model.h5')
-model.summary()
 
-# 学習したモデルに対して予測を行う
-y = model.predict(x)
-print(y)
 
-# 一番大きい番号を出力
-print(np.argmax(y))
+    def predict(self, x):
+        '''
+        判別する
+        '''
+
+        # 学習済みモデルの読み込み
+        model = load_model('keras_cnn_model.h5')
+        # model.summary()
+
+        # 学習したモデルに対して予測を行う
+        y = model.predict(x)
+        print(y)
+
+        print('')
+
+        for result in y:
+
+            print('各ラベルの確率')
+            for num in result:
+                print('%.10f' % num) # 指数表記を読めるようにする
+
+            # 一番大きい番号を出力
+            print('一番大きいラベルを予測結果として判定')
+            print(np.argmax(result))
+            print('')
+
+
+
+    def loadImages(self, imgList):
+        '''
+        画像を読み込む
+        '''
+
+        # 画像サイズ
+        image_w = 32
+        image_h = 32
+
+        x = []
+
+        for i in range(len(imgList)):
+            src = imgList[i]
+
+            img = Image.open(src) # PIL (Pillow(Python Imaging Library))で開く
+            img = img.convert("RGB") # RGB
+            img = img.resize((image_w, image_h)) # リサイズ
+            data = np.asarray(img) # numpyのarrayにする
+            x.append(data)
+
+        x = np.array(x) # 1次元の配列にする
+        # 0〜255を0〜1.0に変換
+        x = x.astype('float32')
+        x /= 255
+
+        return x
+
+
+
+
+if __name__ == '__main__':
+
+    # 引数から画像パスを取得
+    if len(sys.argv) < 2:
+        print("引数に判別する画像のパスを記載してください")
+        sys.exit(1)
+
+    fileList = sys.argv[1:]
+
+    img = ImageClassify()
+
+    # 画像を読み込んで配列にする
+    x = img.loadImages(fileList)
+
+    # 予測
+    img.predict(x)
+
+
